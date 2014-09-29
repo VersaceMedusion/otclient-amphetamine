64136ebe756534ee7b5c3ae4050be43949f9b9d3
Lukasz <ukzz@chemist.com>
2014-09-29 12:52:51 +0200

    Create speak_handler.lua

---
+++ handlers/speak_handler.lua
@@ -0,0 +1,47 @@
+--[[
+  @Authors: Ukzz (Chemik)
+  @Details: Speak handler for module functionality.
+]]
+
+SpeakHandler = {}
+SpeakHandler.settings = {
+	enabled = true,
+	runmode = 'superuser',
+	routing = {
+		private_route = true,
+		default_route = false,
+		dmesg_route = false,
+	},
+	replyies = {
+
+
+	},
+	_callbacks = {
+		default = print"aaaa";
+	},
+	_cache = {}
+}
+
+function SpeakHandler.init()
+  --
+end
+
+function SpeakHandler.terminate()
+  --
+end
+
+function SpeakHandler.processhandler(...)
+	local argc, argv = #{...}, {...};
+  local message_data = string.explode(argv[4], ' "')
+	local messageText, messageParm = message_data[1], message_data[2]
+
+  options = switch{
+    ['help'] = function()   
+        pdmesg("help switch option called!")
+    end,
+    default = function()    
+      
+    end
+  }
+  options:case(messageText)
+end

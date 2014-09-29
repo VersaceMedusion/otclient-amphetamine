46a1adffc09743a0261bff5dbf9b6831c54279f1
Lukasz <ukzz@chemist.com>
2014-09-29 12:24:07 +0200

    Create player.lua

---
+++ classes/player.lua
@@ -0,0 +1,12 @@
+--[[
+  @Authors: Ukzz (Chemik)
+  @Details: Player ext class functions.
+]]
+
+function Player:setOutfitColors(r, g, b)
+  local outfit = self:getOutfit()
+  
+  -- TODO: make it works!
+  
+  return "ok"
+end
